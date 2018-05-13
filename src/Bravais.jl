52ba25074a2fa3fe567482bed85df2c0bec3c501
F Freyer <frederic481994@hotmail.de>
2018-05-13 12:36:39 +0200

    UNIFY constructors

--- src/Bravais.jl
+++ src/Bravais.jl
@@ -22,11 +22,12 @@ function merge(bvs::Vector{Bravais{D, T}}...) where {D, T}
     [b for bv in bvs for b in bv]
 end
 
-# TODO
-# more constructors
 
 # TODO
 # center! for more safety in get_neighbors
+
+
+# Literally just constructors from here on out
 
 ################################################################################
 ### 2D Lattices
@@ -34,7 +35,15 @@ end
 
 
 """
-    square([; pos])
+    square()
+
+Returns a composition of Bravais lattices to represent a square lattice.
+
+Keyword arguments include
+- `pos::Point{2, T} = Point{2}(0.)`: Starting position for this lattice.
+- `a::T = 1.0`: Scaling along the first lattice vector.
+- `b::T = 1.0`: Scaling along the second lattice vector.
+- `isa::Symbol = :cubic`: Lattice shape to use (:cubic or :primitive)
 """
 function square(;
         pos::Point{2, T} = Point{2}(0.),
@@ -54,7 +63,15 @@ end
 
 
 """
-    triangle([, pos])
+    triangle()
+
+Returns a composition of Bravais lattices to represent a triangular lattice.
+
+Keyword arguments include
+- `pos::Point{2, T} = Point{2}(0.)`: Starting position for this lattice.
+- `a::T = 1.0`: Scaling along the first lattice vector.
+- `b::T = 1.0`: Scaling along the second lattice vector.
+- `isa::Symbol = :primitive`: Lattice shape to use (:cubic or :primitive)
 """
 function triangle(;
         pos::Point{2, T} = Point{2}(0.),
@@ -87,7 +104,15 @@ end
 
 
 """
-    honeycomb([, pos])
+    honeycomb()
+
+Returns a composition of Bravais lattices to represent a honeycomb lattice.
+
+Keyword arguments include
+- `pos::Point{2, T} = Point{2}(0.)`: Starting position for this lattice.
+- `a::T = 1.0`: Scaling along the first lattice vector.
+- `b::T = 1.0`: Scaling along the second lattice vector.
+- `isa::Symbol = :primitive`: Lattice shape to use (:cubic or :primitive)
 """
 function honeycomb(;
         pos::Point{2, T} = Point{2}(0.),
@@ -134,19 +159,140 @@ end
 
 
 """
-    fcc([, pos])
+    sc()
+
+Returns a composition of Bravais lattices to represent a simple cubic lattice.
+
+Keyword arguments include
+- `pos::Point{3, T} = Point{3}(0.)`: Starting position for this lattice.
+- `a::T = 1.0`: Scaling along the first lattice vector.
+- `b::T = 1.0`: Scaling along the second lattice vector.
+- `c::T = 1.0`: Scaling along the third lattice vector.
+- `isa::Symbol = :cubic`: Lattice shape to use (:cubic or :primitive)
 """
-fcc(pos = Point{3}(0.)) = Bravais(
-    :primitive,
-    pos,
-    (
-        Vec{3}(0.5, 0.5, 0.0),
-        Vec{3}(0.5, 0.0, 0.5),
-        Vec{3}(0.0, 0.5, 0.5)
-    )
-)
+function sc(;
+        pos::Point{3, T} = Point{3}(0.),
+        a::T = 1.0,
+        b::T = 1.0,
+        c::T = 1.0
+        isa::Symbol = :cubic
+    ) where T <: AbstractFloat
+    [Bravais(
+        :cubic,
+        pos,
+        (
+            a * Vec(1., 0., 0.),
+            b * Vec(0., 1., 0.),
+            b * Vec(0., 0., 1.)
+        )
+    )]
+end
+
 
 """
-    diamond([, pos])
+    bcc()
+
+Returns a composition of Bravais lattices to represent a body centered cubic
+lattice.
+
+Keyword arguments include
+- `pos::Point{3, T} = Point{3}(0.)`: Starting position for this lattice.
+- `a::T = 1.0`: Scaling along the first lattice vector.
+- `b::T = 1.0`: Scaling along the second lattice vector.
+- `c::T = 1.0`: Scaling along the third lattice vector.
+- `isa::Symbol = :primitive`: Lattice shape to use (:cubic or :primitive)
 """
-diamond(pos = Point{3}(0.)) = [fcc(pos), fcc(pos + Point{3}(0.25))]
+function bcc(;
+        pos::Point{3, T} = Point{3}(0.),
+        a::T = 1.0,
+        b::T = 1.0,
+        c::T = 1.0
+        isa::Symbol = :primitive
+    ) where T <: AbstractFloat
+
+    if isa == :cubic
+        return merge(
+            sc(pos = pos, a=a, b=b, c=c, isa = :cubic),
+            sc(pos = pos + Point{3}(0.5), a=a, b=b, c=c, isa = :cubic)
+        )
+    else
+        return [Bravais(
+            :primitive,
+            pos,
+            (
+                a * 0.5 * Vec(1., 1., -1.),
+                b * 0.5 * Vec(1., -1., 1.),
+                b * 0.5 * Vec(-1., 1., 1.)
+            )
+        )]
+    end
+end
+
+
+"""
+    fcc()
+
+Returns a composition of Bravais lattices to represent a face centered cubic
+lattice.
+
+Keyword arguments include
+- `pos::Point{3, T} = Point{3}(0.)`: Starting position for this lattice.
+- `a::T = 1.0`: Scaling along the first lattice vector.
+- `b::T = 1.0`: Scaling along the second lattice vector.
+- `c::T = 1.0`: Scaling along the third lattice vector.
+- `isa::Symbol = :primitive`: Lattice shape to use (:cubic or :primitive)
+"""
+function fcc(;
+        pos::Point{3, T} = Point{3}(0.),
+        a::T = 1.0,
+        b::T = 1.0,
+        c::T = 1.0
+        isa::Symbol = :primitive
+    ) where T <: AbstractFloat
+
+    if isa == :cubic
+        return merge(
+            sc(pos = pos, a=a, b=b, c=c, isa = :cubic),
+            sc(pos = pos + Point(0.5, 0.5, 0.0), a=a, b=b, c=c, isa = :cubic),
+            sc(pos = pos + Point(0.5, 0.0, 0.5), a=a, b=b, c=c, isa = :cubic),
+            sc(pos = pos + Point(0.0, 0.5, 0.5), a=a, b=b, c=c, isa = :cubic)
+        )
+    else
+        return [Bravais(
+            :primitive,
+            pos,
+            (
+                a * 0.5 * Vec(1., 1., 0.),
+                b * 0.5 * Vec(1., 0., 1.),
+                b * 0.5 * Vec(0., 1., 1.)
+            )
+        )]
+    end
+end
+
+
+"""
+    diamond()
+
+Returns a composition of Bravais lattices to represent a diamond lattice.
+
+Keyword arguments include
+- `pos::Point{3, T} = Point{3}(0.)`: Starting position for this lattice.
+- `a::T = 1.0`: Scaling along the first lattice vector.
+- `b::T = 1.0`: Scaling along the second lattice vector.
+- `c::T = 1.0`: Scaling along the third lattice vector.
+- `isa::Symbol = :primitive`: Lattice shape to use (:cubic or :primitive)
+"""
+function diamond(;
+        pos::Point{3, T} = Point{3}(0.),
+        a::T = 1.0,
+        b::T = 1.0,
+        c::T = 1.0
+        isa::Symbol = :primitive
+    ) where T <: AbstractFloat
+
+    merge(
+        fcc(pos = pos, a=a, b=b, c=c, isa=isa),
+        fcc(pos = pos + Point{3}(0.25), a=a, b=b, c=c, isa=isa)
+    )
+end
